b87cd9869b97cbfab9b2c3624cbb56a7c026cf42
Bluscream <Bluscream@users.noreply.github.com>
2026-03-20 20:43:33 +0100

    Feat: Automatically bring dialog to front and set TopMost

--- Source/CLI/Program.cs
+++ Source/CLI/Program.cs
@@ -31,6 +31,19 @@ internal static partial class Program
     [LibraryImport("user32.dll")]
     private static partial IntPtr GetActiveWindow();
 
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool SetForegroundWindow(IntPtr hWnd);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    private const uint SWP_NOSIZE = 0x0001;
+    private const uint SWP_NOMOVE = 0x0002;
+    private const uint SWP_SHOWWINDOW = 0x0040;
+
     [STAThread]
     static void Main(string[] args)
     {
@@ -220,9 +233,21 @@ internal static partial class Program
                 timer.Start();
             }
 
+            var frontTimer = new System.Windows.Forms.Timer { Interval = 100 };
+            frontTimer.Tick += (s, e) => {
+                frontTimer.Stop();
+                var hwnd = GetActiveWindow();
+                if (hwnd != IntPtr.Zero)
+                {
+                    SetForegroundWindow(hwnd);
+                    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+                }
+            };
+            frontTimer.Start();
+
             if (flash)
             {
-                var flashTimer = new System.Windows.Forms.Timer { Interval = 100 };
+                var flashTimer = new System.Windows.Forms.Timer { Interval = 200 };
                 flashTimer.Tick += (s, e) => {
                     flashTimer.Stop();
                     var hwnd = GetActiveWindow();
@@ -260,10 +285,19 @@ internal static partial class Program
             if (!string.IsNullOrEmpty(details)) page.Expander = new TaskDialogExpander { Text = details };
             if (!string.IsNullOrEmpty(checkbox)) page.Verification = new TaskDialogVerificationCheckBox { Text = checkbox };
 
+            page.Created += (s, e) => {
+                var hwnd = GetActiveWindow();
+                if (hwnd != IntPtr.Zero)
+                {
+                    SetForegroundWindow(hwnd);
+                    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+                }
+            };
+
             if (flash)
             {
                 page.Created += (s, e) => {
-                    var flashTimer = new System.Windows.Forms.Timer { Interval = 50 };
+                    var flashTimer = new System.Windows.Forms.Timer { Interval = 100 };
                     flashTimer.Tick += (fs, fe) => {
                         flashTimer.Stop();
                         var hwnd = GetActiveWindow();
